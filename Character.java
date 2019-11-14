a431d66828b43c7a3ce1abcd3f6490e2e333adc9
agrawaladitya222 <55710891+agrawaladitya222@users.noreply.github.com>
2019-11-13 23:10:29 -0500

    Add files via upload

--- Character.java
+++ Character.java
@@ -1,90 +1,182 @@
 package characterprogram;
 
-
 public class Character {
-	private String sentence;
-	private String words[]= new String [25];
-	
+	private String sentence = "";
+	private String words[] = new String[50];
+	private String wordswithoutpunk[] = new String[50];
+	private boolean outputyet[] = new boolean[50];
+
 	public Character() {
-		for(int i=0; i<words.length;i++) {
-			words[i]="";
+		for (int i = 0; i < words.length; i++) {
+			words[i] = "";
 		}
-	
-	
+		for (int j = 0; j < wordswithoutpunk.length; j++) {
+			wordswithoutpunk[j] = "";
+		}
+		for (int k = 0; k < outputyet.length; k++) {
+			outputyet[k] = false;
+		}
+
 	}
-	
+
 	public void getInput(String a) {
 		sentence = a.trim();
+		boolean error = true;
+		if (sentence.equals("")) {
+			throw new IllegalArgumentException();
+		}
+		for (int i = 0; i < sentence.length(); i++) {
+			if (!isSpaceorTab(sentence.charAt(i)) && !isPunk(sentence.charAt(i))) {
+				error = false;
+			}
+		}
+		if (error == true) {
+			throw new IllegalArgumentException();
+		}
 	}
-	
+
 	public int getCharacters() {
-		int count=0;
-		for(int i=0;i<sentence.length();i++) {
-			if(sentence.charAt(i) == '	' || sentence.charAt(i) == ' ') {
+		int count = 0;
+		for (int i = 0; i < sentence.length(); i++) {
+			if (sentence.charAt(i) == '	' || sentence.charAt(i) == ' ') {
 				continue;
-			}else {
+			} else {
 				count = count + 1;
 			}
 		}
 		return count;
 	}
-	
+
 	public void storeWords() {
-		int wordindex=0;
-		for(int i=0;i<sentence.length();i++) {
-			if(isSpaceorTab(sentence.charAt(i)) && isSpaceorTab(sentence.charAt(i-1))) {
+		int wordindex = 0;
+		for (int i = 0; i < sentence.length(); i++) {
+			if (isSpaceorTab(sentence.charAt(i)) && isSpaceorTab(sentence.charAt(i - 1))) {
 				continue;
-			}else if(isSpaceorTab(sentence.charAt(i)) && !isSpaceorTab(sentence.charAt(i-1))) {
-				words[wordindex]=words[wordindex] + String.valueOf(sentence.charAt(i));
-				wordindex= wordindex + 1;
-			}else if(!isSpaceorTab(sentence.charAt(i))) {
-				words[wordindex]=words[wordindex] + String.valueOf(sentence.charAt(i));
+			} else if (isSpaceorTab(sentence.charAt(i)) && !isSpaceorTab(sentence.charAt(i - 1))) {
+				wordindex = wordindex + 1;
+			} else if (!isSpaceorTab(sentence.charAt(i))) {
+				words[wordindex] = words[wordindex] + String.valueOf(sentence.charAt(i));
 			}
 		}
-		
+
 	}
-	
+
 	public int getWords() {
-		int count=0;
-		
-		for(int i=0; i<words.length; i++) {
-			if(!words[i].equals("")) {
+		int count = 0;
+
+		for (int i = 0; i < wordswithoutpunk.length; i++) {
+			if (!wordswithoutpunk[i].equals("")) {
 				count = count + 1;
 			}
 		}
 		return count;
 	}
-	
+
 	public boolean isSpaceorTab(char a) {
-		if(a=='	' || a == ' ') {
+		if (a == ' ' || a == '\t' || a == '\n') {
 			return true;
 		}
 		return false;
 	}
-	
-	public String getRidofPunk(String a) {
-		String result =a;
-		if(!a.equals("") && (a.charAt(a.length()-1)=='.' || a.charAt(a.length()-1)=='!' || a.charAt(a.length()-1)=='?')) {
-			result = a.substring(0, a.length()-2);
+
+	public boolean isPunk(char a) {
+		if (a == '.' || a == '?' || a == '!') {
+			return true;
+		} else {
+			return false;
 		}
+	}
+
+	public String getRidofPunk(String a) {
+		String result;
+
+		for (int i = 0; i < a.length() - 1; i++) {
+			if (isPunk(a.charAt(i)) && !isPunk(a.charAt(i + 1))) {
+				a = a.substring(i + 1);
+				break;
+			}
+		}
+		for (int i = 0; i < a.length() - 1; i++) {
+			if (!isPunk(a.charAt(i)) && isPunk(a.charAt(i + 1))) {
+				a = a.substring(0, i + 1);
+				break;
+			}
+		}
+		result = a;
+
 		return result;
 	}
-	
-	public void tester() {
-		for(int i=0;i<words.length;i++) {
-			if(!words[i].equals("")) {
-			System.out.println(words[i]);
+
+	public void storePunkFreeWords() {
+		for (int i = 0; i < words.length; i++) {
+			if (!words[i].equals("")) {
+				wordswithoutpunk[i] = getRidofPunk(words[i]);
+				if (isOnlyPunk(wordswithoutpunk[i])) {
+					wordswithoutpunk[i] = "";
+				}
 			}
 		}
-		System.out.println(getCharacters());
-		System.out.println(getWords());
-		for(int i=0;i<words.length;i++) {
-			if(!words[i].equals("")) {
-			System.out.println(getRidofPunk(words[i]));
-			}
-		}
-		
-		
 	}
-	
+
+	public boolean isOnlyPunk(String a) {
+		boolean error = true;
+		for (int i = 0; i < a.length(); i++) {
+			if (!isPunk(a.charAt(i))) {
+				error = false;
+			}
+		}
+		return error;
+	}
+
+	public int getAppearances(String a) {
+		int appearances = 0;
+		for (int i = 0; i < wordswithoutpunk.length; i++) {
+			if (wordswithoutpunk[i].equalsIgnoreCase(a)) {
+				appearances = appearances + 1;
+			}
+		}
+		return appearances;
+	}
+
+	public String formOutput() {
+		String output = "";
+		for (int i = 0; i < wordswithoutpunk.length; i++) {
+			if (outputyet[i] == false && !wordswithoutpunk[i].equals("")) {
+				String firstletter = String.valueOf(wordswithoutpunk[i].charAt(0));
+				String restofword = getRestofWord(wordswithoutpunk[i]);
+				firstletter = firstletter.toUpperCase();
+				output = output + firstletter + restofword + "	" + getAppearances(wordswithoutpunk[i]) + "\n";
+				Outputted(wordswithoutpunk[i]);
+			}
+		}
+
+		return output;
+	}
+
+	public String getRestofWord(String a) {
+		String restofword = a.substring(1, a.length());
+		return restofword;
+	}
+
+	public void Outputted(String a) {
+		for (int i = 0; i < wordswithoutpunk.length; i++) {
+			if (wordswithoutpunk[i].equalsIgnoreCase(a)) {
+				outputyet[i] = true;
+			}
+		}
+	}
+
+	public void reset() {
+		for (int i = 0; i < words.length; i++) {
+			words[i] = "";
+		}
+		for (int j = 0; j < wordswithoutpunk.length; j++) {
+			wordswithoutpunk[j] = "";
+		}
+		for (int k = 0; k < outputyet.length; k++) {
+			outputyet[k] = false;
+		}
+		sentence = "";
+
+	}
 }
--- CharacterGUI.java
+++ CharacterGUI.java
@@ -3,38 +3,58 @@ package characterprogram;
 import javax.swing.*;
 import BreezySwing.*;
 
-public class CharacterGUI extends GBFrame{
+public class CharacterGUI extends GBFrame {
 	private JButton calculate;
 	private JLabel inputlabel;
 	private JTextArea area;
+	private JButton reset;
 	private Character character = new Character();
-	
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame gui = new CharacterGUI();
 		gui.setTitle("Word Counter");
 		gui.setVisible(true);
-		gui.setSize(800,400);
+		gui.setSize(800, 400);
 	}
-	
+
 	public CharacterGUI() {
-		calculate = addButton("Calculate",3,1,1,1);
-		inputlabel = addLabel("Input a sentence:",1,1,1,1);
-		area = addTextArea("",2,1,1,1);
-		
+		calculate = addButton("Calculate", 3, 1, 1, 1);
+		inputlabel = addLabel("Input a sentence:", 1, 1, 1, 1);
+		area = addTextArea("", 2, 1, 1, 1);
+		reset = addButton("Reset", 4, 1, 1, 1);
+		area.requestFocus();
+		reset.setEnabled(false);
+
 	}
-	
+
 	public void buttonClicked(JButton button) {
-		if(button == calculate) {
-			character.getInput(area.getText());
-			character.storeWords();
-			character.tester();
+		if (button == calculate) {
+			try {
+				character.getInput(area.getText());
+				character.storeWords();
+				character.storePunkFreeWords();
+				area.setText(area.getText() + "\nCharacters: " + character.getCharacters() + "\nWords: "
+						+ character.getWords() + "\n" + character.formOutput());
+				calculate.setEnabled(false);
+				reset.setEnabled(true);
+			} catch (IllegalArgumentException e) {
+				messageBox(
+						"Error.The input needs to contain at least one character other than punctuation, spaces/tabs, or nothing.");
+				character.reset();
+				area.setText("");
+				area.requestFocus();
+				reset.setEnabled(false);
+				calculate.setEnabled(true);
+			}
+		} else if (button == reset) {
+			character.reset();
+			area.setText("");
+			area.requestFocus();
+			calculate.setEnabled(true);
+			reset.setEnabled(false);
 		}
-		
-		
-		
-		
+
 	}
 
 }
